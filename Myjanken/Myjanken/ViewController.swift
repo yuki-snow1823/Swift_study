0c69a3a8c7fe7d0e32b60830f6d822cf3f36ec0a
yuki-snow1823 <magic.iroha@gmail.com>
2020-08-01 19:07:35 +0900

    ジャンケンゲームの作成開始

---
+++ Myjanken/Myjanken/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Myjanken
+//
+//  Created by Horikoshi Yuki on 2020/08/01.
+//  Copyright © 2020 優希 堀越. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
