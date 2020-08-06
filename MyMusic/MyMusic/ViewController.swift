62a5db6ac8350f475dce1162843b5fd65c7bfb3d
yuki-snow1823 <magic.iroha@gmail.com>
2020-08-06 18:03:51 +0900

    新プロジェクト

---
+++ MyMusic/MyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by Horikoshi Yuki on 2020/08/06.
+//  Copyright © 2020 優希 堀越. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
